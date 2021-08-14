9d92c8e0da4971402ea99fe9b1c14b91992fa8a3
Sweyd Abdul <abdulsweyd@gmail.com>
2021-08-14 19:50:16 +0200

    Documentação  de funções

---
+++ Eduardo Mendes/Minissérie Pythonica/exemplo_5.py
@@ -0,0 +1,15 @@
+"""Documentaćão  de funćões"""
+
+
+def soma(x: int, y: int) -> int:
+    # PEP - 257
+    """ Soma 2 tipos somáveis.
+
+    :rtype: inteiro
+    :param x: Objeto somável.
+    :param y: Objeto somável.
+    :return: soma de dois somáveis.
+    """
+
+
+print(soma.__doc__)
